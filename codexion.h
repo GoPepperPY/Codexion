55944edda5cff5ee586c78d6c82b43e292ad21f7
Goncalo Pimenta <gopiment@c2r1s9.42porto.com>
2026-09-16 14:35:44 +0100

    dasd

--- codexion.h
+++ codexion.h
@@ -6,12 +6,22 @@
 /*   By: gopiment <gopiment@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/14 19:47:31 by gopiment          #+#    #+#             */
-/*   Updated: 2026/09/14 19:49:41 by gopiment         ###   ########.fr       */
+/*   Updated: 2026/09/16 13:44:20 by gopiment         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CODEXION_H
 # define CODEXION_H
 
+typedef struct s_codex{
+	int		coders;
+	int		time_to_bo;
+	int		time_to_compile;
+	int		time_to_debug;
+	int		time_to_refactor;
+	int 	number_of_compiles_required;
+	int 	dongle_cooldown;
+	char	*scheduler;
+}				t_codex;
 
 # endif
